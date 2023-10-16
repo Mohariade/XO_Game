6b5d1526919bd36bdb287bff1110c1b3fd310118
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-10-16 22:59:04 +0100

    Creating 3 Tier Architecture Templet

---
+++ Data_Access_Layer/DataAccessSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+
+namespace Data_Access_Layer
+{
+    public class clsDataAccessSettings
+    {
+        static public string ConnectionString { get; } = "CONNECTION_STRING_HERE";
+    }
+}
