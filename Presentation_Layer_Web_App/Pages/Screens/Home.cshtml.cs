32ecd8f65f16339c549b03a0b7744bac9ed6df1a
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-10-17 21:50:38 +0100

    Creating Home and Game Over Pages

---
+++ Presentation_Layer_Web_App/Pages/Screens/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation_Layer_Web_App.Pages.Screens
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
